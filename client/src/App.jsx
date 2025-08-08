569c5aedfb917d244f774a8d2b8ebc50a9317915
joshuainfluence <joshuajulius2030@gmail.com>
2025-08-08 12:23:46 +0100

    login page modified route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,15 +13,16 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route index element={<Feed />} />
-        <Route path="messages" element={<Messages />} />
-        <Route path="messages/:userId" element={<ChatBox />} />
-        <Route path="connections" element={<Connections />} />
-        <Route path="discover" element={<Discover />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="profile/:profileId" element={<Profile />} />
-        <Route path="create-post" element={<CreatePost />} />
+        <Route path="/" element={<Login />}>
+          <Route index element={<Feed />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="messages/:userId" element={<ChatBox />} />
+          <Route path="connections" element={<Connections />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile/:profileId" element={<Profile />} />
+          <Route path="create-post" element={<CreatePost />} />
+        </Route>
       </Routes>
     </>
   );
