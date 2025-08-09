f0e396979ace5a387f8666b1d1b1bf584c344973
joshuainfluence <joshuajulius2030@gmail.com>
2025-08-09 05:26:53 +0100

    loading component

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,11 +1,13 @@
 import { Menu, Sidebar, X } from "lucide-react";
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { dummyUserData } from "../assets/assets";
+import Loading from "../components/Loading";
 
 const Layout = () => {
   const [sidebarOPen, setSidebarOPen] = useState(false);
-  
-  return (
+  const user = dummyUserData
+  return user ?(
     <div className="w-full flex h-screen">
       <Sidebar />
       <div className="flex-1 bg-slate-50">
@@ -18,6 +20,8 @@ const Layout = () => {
         <Menu className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden" onClick={()=>setSidebarOPen(true)}/>
       }
     </div>
+  ) : (
+   <Loading />
   );
 };
 
