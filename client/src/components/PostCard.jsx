c5d6bcad430d4154501ef664be7a6559235f26bb
joshuainfluence <joshuajulius2030@gmail.com>
2025-08-15 03:54:49 +0100

    displaying user posts with actions on feeds page

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,8 +1,16 @@
-import { BadgeCheck } from "lucide-react";
+import { BadgeCheck, Heart, MessageCircle, Share, Share2 } from "lucide-react";
 import moment from "moment";
-import React from "react";
+import React, { useState } from "react";
+import { dummyUserData } from "../assets/assets";
 
 const PostCard = ({ post }) => {
+  const postWithHashtags = post.content.replace(
+    /(#\w+)/g,
+    '<span class="text-indigo-600">$1</span>'
+  );
+  const [likes, setLikes] = useState(post.likes_count);
+  const currentUser = dummyUserData;
+  const handleLike = () => {};
   return (
     <div className="bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl ">
       {/* user info */}
@@ -18,8 +26,51 @@ const PostCard = ({ post }) => {
             <BadgeCheck className="w-4 h-4 text-blue-500" />
           </div>
           <div className="text-sm text-gray-500">
-            @{post.user.username}  {moment(post.createdAt).fromNow()}
+            @{post.user.username} {moment(post.createdAt).fromNow()}
           </div>
+        </div>
+      </div>
+      {/* post content */}
+      {post.content && (
+        <div
+          className="text-gray-800 whitespace-pre-line text-sm"
+          dangerouslySetInnerHTML={{ __html: postWithHashtags }}
+        />
+      )}
+      {/* images */}
+      <div className="grid grid-cols-2 gap-2">
+        {post.image_urls.map((img, index) => (
+          <img
+            src={img}
+            key={index}
+            className={`w-full h-48 object-cover rounded-lg ${
+              post.image_urls.length === 1 && "col-span-2 h-auto "
+            }`}
+            alt=""
+          />
+        ))}
+      </div>
+
+      {/* actions */}
+      <div className="flex items-center gap-4 text-gray-600 text-sm pt-2 border-t border-gray-300">
+        <div className="flex items-center gap-1">
+          <Heart
+            className={`w-4 h-4 cursor-pointer ${
+              likes.includes(currentUser._id) && "text-red-500 fill-red-500"
+            }`}
+            onClick={handleLike}
+          />
+          <span>{likes.length}</span>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <MessageCircle className={`w-4 h-4 cursor-pointer `} />
+          <span>{12}</span>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <Share2 className={`w-4 h-4 cursor-pointer `} />
+          <span>{7}</span>
         </div>
       </div>
     </div>
@@ -27,4 +78,3 @@ const PostCard = ({ post }) => {
 };
 
 export default PostCard;
-
