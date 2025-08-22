e97d17b0a04c7996d0ca65b707b13980ea701a32
joshuainfluence <joshuajulius2030@gmail.com>
2025-08-22 06:16:27 +0100

    connections page, issue with maping out dataArray

--- client/src/pages/Connections.jsx
+++ client/src/pages/Connections.jsx
@@ -53,7 +53,7 @@ const Connections = () => {
         <div className="inline-flex flex-wrap items-center border border-gray-200 rounded-md p-1 bg-white shadow-sm">
           {dataArray.map((tab) => (
             <button
-            onClick={()=> setCurrentTab(tab.label)}
+              onClick={() => setCurrentTab(tab.label)}
               key={tab.label}
               className={`cursor-pointer flex items-center text-sm rounded-md px-3 py-1 transition-colors ${
                 currentTab === tab.label
@@ -70,6 +70,70 @@ const Connections = () => {
               )}
             </button>
           ))}
+        </div>
+
+        {/* connections */}
+        {/* <div className="flex flex-wrap gap-6 mt-6">
+          {dataArray
+            .find((item) => item.label === currentTab)
+            .value.map((user) => (
+              <div
+                key={user._id}
+                className="w-full max-w-88 flex gap-5 p-6 bg-white shadow rounded-md"
+              >
+                <img
+                  src={user.profile_picture}
+                  className="rounded-full w-12 h-12 shadow-md mx-auto"
+                  alt=""
+                />
+                <div className="flex-1">
+                  <p className="font-medium text-slate-700">{user.full_name}</p>
+                  <p className="text-slate-500">@{user.username}</p>
+                  <p className="text-sm text-gray-600">
+                    @{user.bio.slice(0, 30)}...
+                  </p>
+                  <div className="flex max-sm:flex-col gap-2 mt-4">
+                    {
+                      <button
+                        onClick={() => navigate(`/profile/${user._id}`)}
+                        className="w-full p-2 text-sm rounded bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition text-white cursor-pointer"
+                      >
+                        View Profile
+                      </button>
+                    }
+
+                    {currentTab === "Following" && (
+                      <button className="w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-black active:scale-95 transition cursor-pointer">
+                        Unfollow
+                      </button>
+                    )}
+
+                    {currentTab === "Pending" && (
+                      <button className="w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-black active:scale-95 transition cursor-pointer">
+                        Accept
+                      </button>
+                    )}
+
+                    {currentTab === "Connections" && (
+                      <button
+                        onClick={() => navigate(`/messages/${user._id}`)}
+                        className="w-full p-2 text-sm rounded bg-slate-100 hover:bg-slate-200 text-black active:scale-95 transition cursor-pointer flex items-center justify-center gap-1"
+                      >
+                        <MessagesSquare className="w-4 h-4" />
+                        Message
+                      </button>
+                    )}
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div> */}
+
+        <div className="flex flex-wrap gap-6 mt-6">
+          {dataArray.find((item)=>item.label === currentTab).map((user)=>(
+            <div>{user.full_name}</div>
+          ))}
+          <h1>hello</h1>
         </div>
       </div>
     </div>
