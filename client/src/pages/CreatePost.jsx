49cd51fb21a3e8227351ceac2a50686ee808d91a
joshuainfluence <joshuajulius2030@gmail.com>
2025-08-25 06:36:42 +0100

    image upload, show preview, cancel and upload multiple images

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { dummyUserData } from "../assets/assets";
-import { X } from "lucide-react";
+import { Image, X } from "lucide-react";
 
 const CreatePost = () => {
   const [content, setContent] = useState("");
@@ -43,7 +43,42 @@ const CreatePost = () => {
             value={content}
           />
 
-          
+          {/* images */}
+          {images.length > 0 && (
+            <div className="flex flex-wrap gap-2 mt-4">
+              {images.map((image, i) => (
+                <div key={i} className="relative group">
+                  <img
+                    src={URL.createObjectURL(image)}
+                    className="h-20 rounded-md"
+                    alt=""
+                  />
+                  <div
+                    onClick={() =>
+                      setImages(images.filter((_, index) => index !== i))
+                    }
+                    className="absolute hidden group-hover:flex justify-center items-center top-0 right-0 bottom-0 left-0 bg-black/40 rounded-md cursor-pointer"
+                  >
+                    <X className="w-6 h-6 text-white" />
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+
+          {/* bottom bar */}
+          <div className="flex items-center justify-between pt-3 border-t border-gray-300">
+            <label
+              htmlFor="images"
+              className="flex items-center gap-2 text-sm text-gray-500 hover:text-gray-700 transition cursor-pointer"
+            >
+              <Image className="size-6" />
+            </label>
+            <input type="file" name="" id="images" accept="image/*" hidden multiple onChange={(e)=>setImages([...images, ...e.target.files])} />
+            <button className="text-sm bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition text-white font-medium px-8 py-2 rounded-md cursor-pointer">
+              Publish Post
+            </button>
+          </div>
         </div>
       </div>
     </div>
