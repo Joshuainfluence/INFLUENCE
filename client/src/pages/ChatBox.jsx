32cc078926a3022f9c839be9c56d8df3868b2b92
joshuainfluence <joshuajulius2030@gmail.com>
2025-08-25 23:58:45 +0100

    message incoming

--- client/src/pages/ChatBox.jsx
+++ client/src/pages/ChatBox.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import { dummyMessagesData, dummyUserData } from "../assets/assets";
 
 const ChatBox = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const messages = dummyMessagesData;
+  const [text, setText] = useState("");
+  const [image, setImage] = useState(null);
+  const [user, setUser] = useState(dummyUserData);
+  const messagesEndRef = useRef(null);
 
-export default ChatBox
+  const sendMessage = async () => {};
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+  return (
+    user && (
+      <div className="flex flex-col h-screen">
+        <div className="flex items-center gap-2 p-2 md:px-10 xl:pl-42 bg-gradient-to-r from-indigo-50 to-purple-50 border-b border-gray-300">
+          <img
+            src={user.profile_picture}
+            className="size-8 rounded-full"
+            alt=""
+          />
+          <div>
+            <p className="font-medium">{user.full_name}</p>
+            <p className="text-sm text-gray-500 -mt-1.5">@{user.username}</p>
+          </div>
+        </div>
+
+        <div className="p-5 md:px-10 h-full overflow-y-scroll">
+          <div className="space-y-4 max-w-4xl mx-auto">
+            {messages.toSorted((a, b)=>new Date(a.createdAt) - new Date(b.createdAt)).map((message, index)=>(
+              <div key={index} className={`flex flex-col ${message.to_user_id !== user._id ? 'items-start' : 'items-end'}`}>
+<div>
+  <p>{message.text}</p>
+</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default ChatBox;
